9be57c420126f82004615eeacb1f3b399af7d0ff
sumeetbidhan <sumeetbidhanwork@gmail.com>
2024-07-20 15:34:36 +0530

    update_backend

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const cors = require('cors'); // Add this line
+const cors = require('cors');
 const app = express();
 const port = 3001;
 
-app.use(cors()); // Add this line
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
 
 app.get('/globe-data', (req, res) => {
   const dataPath = path.join(__dirname, 'data.json');
   fs.readFile(dataPath, 'utf8', (err, data) => {
     if (err) {
+      console.error('Error reading data file:', err);
       res.status(500).send('Error reading data file');
       return;
     }
